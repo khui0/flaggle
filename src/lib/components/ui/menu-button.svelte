6fc33360db3c3531b2207e38c1d815f8d3039c2c
Kenny <101839505+khui0@users.noreply.github.com>
2025-06-08 02:03:35 -0400

    feat: add shadow to menu buttons

--- src/lib/components/ui/menu-button.svelte
+++ src/lib/components/ui/menu-button.svelte
@@ -31,14 +31,14 @@
   <a
     {href}
     {onclick}
-    class="font-title group btn btn-lg rounded-box relative p-10 max-md:last:col-span-2"
+    class="font-title group btn btn-lg shadow-lg rounded-box relative p-10 max-md:last:col-span-2"
   >
     {@render content()}
   </a>
 {:else}
   <button
     {onclick}
-    class="font-title group btn btn-lg rounded-box relative p-10 max-md:last:col-span-2"
+    class="font-title group btn btn-lg shadow-lg rounded-box relative p-10 max-md:last:col-span-2"
   >
     {@render content()}
   </button>
