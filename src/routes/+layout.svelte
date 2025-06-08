0535a52c644ad089427711461a50d911643829ba
Kenny <101839505+khui0@users.noreply.github.com>
2025-06-08 02:02:00 -0400

    feat: tweaks

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -32,19 +32,22 @@
     class="fixed inset-0 z-0 flex items-center justify-center overflow-hidden"
   >
     <FlagBackground />
+  <div class="bg-accent absolute bottom-0 left-0 z-0 h-1/5 w-full mask-t-from-0%"></div>
   </div>
   <div
     in:fade
     out:fade
-    class="fixed inset-0 z-10 flex flex-col items-center justify-center gap-5 p-10"
+    class="fixed inset-0 z-10 flex flex-col items-center justify-center gap-5 p-5"
   >
-    <div class="flex items-center gap-5">
+    <div class="flex items-center gap-3 md:gap-5">
       <enhanced:img
         src="$lib/assets/branding/flaggle.svg"
-        class="h-19 w-19 drop-shadow-lg drop-shadow-black/50"
+        class="h-13 w-auto drop-shadow-lg md:h-19"
         alt="Flaggle icon"
       />
-      <h1 class="font-title text-8xl leading-none text-shadow-black/50 text-shadow-lg">Flaggle</h1>
+      <h1 class="font-title text-6xl leading-none text-white text-shadow-lg md:text-8xl">
+        Flaggle
+      </h1>
     </div>
     <div class="grid w-full max-w-4xl grid-cols-2 gap-2 md:grid-cols-5">
       <MenuButton href="/daily" label="Daily">
@@ -64,12 +67,19 @@
       </MenuButton>
     </div>
   </div>
+  <div
+    class="fixed right-0 bottom-0 left-0 z-20 m-3 flex justify-between leading-none font-semibold text-white mix-blend-overlay"
+  >
+    <p>
+      <a href="http://kennyhui.dev" target="_blank" rel="noopener noreferrer">kennyhui.dev</a>
+    </p>
+    <p>
+      {import.meta.env.PACKAGE_VERSION}
+    </p>
+  </div>
 {:else}
   <div class="mx-auto flex w-full max-w-4xl flex-col gap-4 p-4 pb-16">
     {@render children()}
   </div>
   <Navigation />
 {/if}
-<p class="fixed right-0 bottom-0 m-3 leading-none font-semibold mix-blend-overlay">
-  {import.meta.env.PACKAGE_VERSION}
-</p>
