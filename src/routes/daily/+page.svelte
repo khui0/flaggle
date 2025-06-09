b8c8ad879ca0e4fb612a2871125d17390619d765
Kenny <101839505+khui0@users.noreply.github.com>
2025-06-09 11:26:50 -0400

    feat: rework daily mode

--- src/routes/daily/+page.svelte
+++ src/routes/daily/+page.svelte
@@ -1,79 +1,82 @@
 <script lang="ts">
+  import { browser } from "$app/environment";
   import data from "$lib/assets/flags/data.json";
   import CopyButton from "$lib/components/ui/copy-button.svelte";
   import GameContainer from "$lib/components/ui/game-container.svelte";
   import ClassicFeed from "$lib/components/widgets/classic-feed.svelte";
   import FlagInput from "$lib/components/widgets/flag-input.svelte";
-  import type { Flag } from "$lib/content";
-  import { getDeltaDay } from "$lib/date";
+  import { flags, type Flag } from "$lib/content";
   import { db } from "$lib/db";
   import { generateDiff } from "$lib/diff";
-  import { dailyStreak } from "$lib/stats";
+  import { dailyStats } from "$lib/stats";
   import { sha256 } from "@oslojs/crypto/sha2";
+  import dayjs from "dayjs";
   import { liveQuery } from "dexie";
   import pluralize from "pluralize";
   import { onMount } from "svelte";
+  import { fade } from "svelte/transition";
 
   interface Guess extends Flag {
     diff?: string;
     win?: boolean;
   }
 
-  const dailyNumber = getDeltaDay("2024-06-06") * -1 + 1;
+  interface GameState {
+    date: string;
+    target: Flag | null;
+    guesses: Guess[];
+  }
 
-  const date = new Date().toLocaleDateString("en-US", {
-    month: "numeric",
-    day: "2-digit",
-    year: "numeric",
+  const defaultGameState: GameState = {
+    date: dayjs().format("YYYY-MM-DD"),
+    target: null,
+    guesses: [],
+  };
+
+  const storedGameState: GameState =
+    browser && JSON.parse(localStorage.getItem("daily-game-state") || "{}");
+
+  let gameState: GameState = $state(Object.assign({}, defaultGameState, storedGameState));
+
+  $effect(() => {
+    localStorage.setItem("daily-game-state", JSON.stringify(gameState));
   });
 
-  let ISODate: string;
+  const streak = dailyStats.streak;
 
-  // Game state
-  let target: Flag | null = $state(null);
-  let items: Guess[] = $state([]);
-  let isGameOver: boolean = $state(false);
-  let guesses: number = $state(0);
-  let solved: boolean = $state(false);
-
-  const daily = liveQuery(() => db.daily.get(ISODate));
-
-  let shareString = $derived(
-    `🏁 Flaggle #${dailyNumber} ${date} in ${pluralize("guess", $daily?.guesses || guesses, true)}! 👉 https://flaggle.kennyhui.dev/daily`,
+  const previous = liveQuery(() => db.daily.get(dailyFlaggleDate));
+  const dailyFlaggleNumber = dayjs().diff(dayjs("2024-06-06"), "day") + 1;
+  const dailyFlaggleDate = $derived(dayjs().format("YYYY-MM-DD"));
+  const shareString = $derived(
+    `🏁 Flaggle #${dailyFlaggleNumber} ${dayjs().format("l")} in ${pluralize("guess", $previous?.guesses || gameState.guesses.length, true)}! 👉 https://flaggle.kennyhui.dev/daily`,
   );
 
-  onMount(async () => {
-    // Generate a random number by hashing date and using the first 3 characters as a hex number
-    const tzo = new Date().getTimezoneOffset() * 60_000;
-    const date = new Date(Date.now() - tzo).toISOString().split("T")[0];
-    ISODate = date;
-    const hash = await sha256(new TextEncoder().encode(date));
-    const rnd = (hash[0] + hash[1]) / (255 * 2);
-    const index = Math.floor(rnd * data.length);
-    target = data[index];
+  let solved: boolean = $state(false);
 
-    // Fill previously guessed items
-    const previouslyGuessed: Guess[] = JSON.parse(
-      localStorage.getItem("daily-prev-guessed") || "[]",
-    );
-    const previouslyGuessedDate = localStorage.getItem("daily-prev-guessed-date");
-    // Different day; reset
-    if (!previouslyGuessedDate || previouslyGuessedDate != ISODate) {
-      localStorage.removeItem("daily-prev-guessed");
-    } else if (previouslyGuessed) {
-      items = previouslyGuessed;
+  previous.subscribe((record) => {
+    if (record?.guesses) solved = true;
+  });
+
+  onMount(async () => {
+    gameState.target = getDailyTarget();
+
+    // Reset state if it's for a different date
+    if (gameState.date !== dailyFlaggleDate) {
+      gameState = Object.assign({}, defaultGameState);
     }
   });
 
-  // Show results if today has already been played
-  daily.subscribe((daily) => {
-    if (daily?.guesses) solved = true;
-  });
+  function getDailyTarget(): Flag {
+    const hash = sha256(new TextEncoder().encode(dailyFlaggleDate));
+    const rnd = (hash[0] + hash[1]) / (255 * 2);
+    const index = Math.floor(rnd * data.length);
+    return flags[index];
+  }
 
   async function addGuess(flag: Flag) {
-    if (isGameOver) return;
-    if (target === null) return;
-    const diff = await generateDiff(flag, target);
+    if (gameState.target === null) return;
+
+    const diff = await generateDiff(flag, gameState.target);
     const win = checkWin(flag);
     const guess: Guess = {
       code: flag.code,
@@ -81,48 +84,50 @@
       diff: diff,
       win: win,
     };
-    items = [guess, ...items];
-    guesses++;
+    gameState.guesses = [guess, ...gameState.guesses];
+
     if (win) {
-      // Record today as win if there is not existing record
-      const exists = (await db.daily.get(ISODate)) !== undefined;
-      if (!exists) db.daily.put({ date: ISODate, guesses: items.length }, ISODate);
-      // Show results modal
       solved = true;
+      // Record today's game as win if there is not existing record
+      if ($previous === undefined)
+        db.daily.put(
+          { date: dailyFlaggleDate, guesses: gameState.guesses.length },
+          dailyFlaggleDate,
+        );
     }
-    // Store guess history
-    localStorage.setItem("daily-prev-guessed", JSON.stringify(items));
-    localStorage.setItem("daily-prev-guessed-date", ISODate);
   }
 
   function checkWin(guess: Flag): boolean {
-    if (target?.code === guess.code) {
-      isGameOver = true;
+    if (gameState.target === null) return false;
+    if (gameState.target.code === guess.code) {
       return true;
     }
     return false;
   }
 </script>
 
+<GameContainer>
+  {#snippet title()}
+    Flaggle <span class="text-base-content/50">#{dailyFlaggleNumber}</span>
+  {/snippet}
+  {#snippet header()}
+    {#if !$previous?.guesses}
+      <FlagInput onsubmit={addGuess} />
+    {/if}
+  {/snippet}
+  <ClassicFeed items={gameState.guesses} />
+</GameContainer>
+
 {#if solved}
-  <div class="bg-base-100 absolute inset-0 flex flex-col items-center justify-center gap-4">
-    <p>You solved today's <b>Flaggle #{dailyNumber}</b> in</p>
-    <p class="mb-2 font-[BigNoodleTitling] text-5xl italic">
-      {pluralize("guess", $daily?.guesses || guesses, true)}
+  <div
+    in:fade={{ delay: 1000 }}
+    class="bg-base-100 absolute inset-0 flex flex-col items-center justify-center gap-4"
+  >
+    <p>You solved today's <b>Flaggle #{dailyFlaggleNumber}</b> in</p>
+    <p class="font-title mb-2 text-5xl">
+      {pluralize("guess", $previous?.guesses || gameState.guesses.length, true)}
     </p>
-    <p>You now have a <b>{$dailyStreak} day</b> streak!</p>
+    <p>You now have a <b>{$streak} day</b> streak!</p>
     <CopyButton content={shareString}>Copy Results</CopyButton>
   </div>
-{:else}
-  <GameContainer>
-    {#snippet title()}
-      Flaggle <span class="text-base-content/50">#{dailyNumber}</span>
-    {/snippet}
-    {#snippet header()}
-      {#if !(isGameOver || $daily?.guesses)}
-        <FlagInput onsubmit={addGuess}></FlagInput>
-      {/if}
-    {/snippet}
-    <ClassicFeed {items}></ClassicFeed>
-  </GameContainer>
 {/if}
